9d91950a75c4737397394b1bd525e696c05eb055
sandarovich <sandarovich1@ukr.net>
2016-05-04 02:10:35 +0300

    //TODO JAX-RS Client

--- tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/Callable.java
+++ tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/Callable.java
@@ -3,4 +3,8 @@ package com.kolodiazhny.chalenge;
 
 public interface Callable {
     String getTaskInfo(int taskId);
+
+    String getCurrentTaskInfo();
+
+    void startTask(int taskId);
 }
--- tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/TransferWiseNetClient.java
+++ tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/TransferWiseNetClient.java
@@ -9,14 +9,16 @@ import java.net.URL;
 
 public class TransferWiseNetClient implements Callable {
 
-    private static final String MY_TOKEN = "?token=6ee5d540d9855947dee6040451089dd1c783f97b";
+    public static final int CURRENT_TASK_ID = 0;
+    private static final String INDIVIDUAL_TOKEN = "?token=6ee5d540d9855947dee6040451089dd1c783f97b";
     private static final String DESTINATION_URL = "http://bootcamp-api.transferwise.com/task/";
 
+    //http://www.mkyong.com/webservices/jax-rs/restful-java-client-with-jersey-client/
     @Override
     public String getTaskInfo(int taskId) {
         String result = "";
         try {
-            URL url = new URL(DESTINATION_URL + taskId + MY_TOKEN);
+            URL url = getUrlByTaskId(taskId);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
@@ -38,6 +40,23 @@ public class TransferWiseNetClient implements Callable {
             return "Server error: ";
         }
         return result;
+    }
+
+    private URL getUrlByTaskId(int taskId) throws MalformedURLException {
+        if (taskId == CURRENT_TASK_ID) {
+            return new URL(DESTINATION_URL + INDIVIDUAL_TOKEN);
+        }
+        return new URL(DESTINATION_URL + taskId + INDIVIDUAL_TOKEN);
+    }
+
+    @Override
+    public String getCurrentTaskInfo() {
+        return getTaskInfo(CURRENT_TASK_ID);
+    }
+
+    @Override
+    public void startTask(int taskId) {
+
     }
 
 }
