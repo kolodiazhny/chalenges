608c4feeb31e32266a3b30605eb325edc12340f5
sandarovich <sandarovich1@ukr.net>
2016-05-04 01:03:12 +0300

    Get Listener ftom Url

--- tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/NetClient.java
+++ tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/NetClient.java
@@ -1,11 +1,11 @@
 package com.kolodiazhny.chalenge;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class NetClient {
 
