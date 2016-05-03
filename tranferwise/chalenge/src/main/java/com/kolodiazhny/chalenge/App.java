201a04ba0177152598c2a673df899c5d963d30ae
sandarovich <sandarovich1@ukr.net>
2016-05-04 01:02:23 +0300

    Get Listener ftom Url

---
+++ tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/App.java
@@ -0,0 +1,7 @@
+package com.kolodiazhny.chalenge;
+
+public class App {
+    public static void main(String[] args) {
+        new NetClient().start();
+    }
+}
---
+++ tranferwise/chalenge/src/main/java/com/kolodiazhny/chalenge/NetClient.java
@@ -0,0 +1,43 @@
+package com.kolodiazhny.chalenge;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NetClient {
+
+    private static final String MY_TOKEN = "?token=6ee5d540d9855947dee6040451089dd1c783f97b";
+    private static final String DESTINATION_URL = "http://bootcamp-api.transferwise.com/task/1";
+
+    public void start() {
+        try {
+            URL url = new URL(DESTINATION_URL + MY_TOKEN);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
